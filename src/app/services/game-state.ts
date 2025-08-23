fbdd5e4cf93d44c78baca7e4e9ae5f40ddf5032e
JonahThurston <jonah.m.thurston@gmail.com>
2025-08-23 17:00:30 -0600

    set up scene system

--- src/app/services/game-state.ts
+++ src/app/services/game-state.ts
@@ -1,19 +1,45 @@
-import { Injectable, signal } from '@angular/core';
-import { FlagRecord, initialFlags } from './records/flag-record';
-import { initialResources, ResourceRecord } from './records/resources-record';
+import { inject, Injectable, linkedSignal, signal } from '@angular/core';
+import { FlagRecord, initialFlags } from './types/flag-record';
+import { initialResources, ResourceRecord } from './types/resources-record';
 import {
   initialRelationships,
   RelationshipRecord,
-} from './records/relationship-record';
+} from './types/relationship-record';
+import { Scene } from './types/scene';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameState {
-  public questNumber = signal<number>(0);
+  public questTitle = signal<string>('prologue');
   public sceneNumber = signal<number>(0);
 
   public flags = signal<FlagRecord>(initialFlags);
   public resources = signal<ResourceRecord>(initialResources);
   public relationships = signal<RelationshipRecord>(initialRelationships);
+
+  public currentScene = signal<Scene | undefined>(undefined);
+
+  private http = inject(HttpClient);
+
+  public procedeToScene(sceneId: number) {
+    if (sceneId === -1) {
+      this.questTitle.set('overworld');
+      this.sceneNumber.set(0);
+    } else {
+      this.sceneNumber.set(sceneId);
+    }
+
+    const url = `/dialogue/${this.questTitle()}/scene${this.sceneNumber()}.json`;
+    let sceneJson: any;
+
+    this.http.get(url).subscribe((res) => {
+      sceneJson = res;
+    });
+
+    const sceneObj = JSON.parse(sceneJson);
+    //TODO: verify that sceneObj is  scene
+    this.currentScene.set(sceneObj);
+  }
 }
---
+++ src/app/services/types/scene.ts
@@ -0,0 +1,31 @@
+import { FlagRecord } from './flag-record';
+import { RelationshipRecord } from './relationship-record';
+import { ResourceRecord } from './resources-record';
+
+//quest has many scenes
+//scenes have onEnter effects, many messages, and multiple choices
+//effects have a list of resource names and numbers, and a list of relationship names and numbers
+//messages have a speaker, text, and an optional picture
+//choices have text, an optional stat + difficulty, an optional flag lock, and a nextScene link
+export type Scene = {
+  id: number;
+  prevSceneId: number;
+  effects?: {
+    resourceEffects?: ResourceRecord;
+    relationshipEffects?: RelationshipRecord;
+  };
+  messages: Message[];
+  question: Choice[];
+};
+
+type Message = {
+  speaker: string;
+  text: string;
+};
+
+type Choice = {
+  text: string;
+  skill?: [string, 'easy' | 'medium' | 'hard' | 'daunting'];
+  flagLock?: FlagRecord;
+  nextSceneId: number;
+};
